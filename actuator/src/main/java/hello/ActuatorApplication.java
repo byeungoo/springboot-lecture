34d36c26ea63c12b8d1921a866039b28dd16f41e
byeungoo <dksekfldks65@naver.com>
2023-03-19 21:36:26 +0900

    actuator 예제 진행

--- actuator/src/main/java/hello/ActuatorApplication.java
+++ actuator/src/main/java/hello/ActuatorApplication.java
@@ -1,13 +1,24 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ActuatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ActuatorApplication.class, args);
+    }
+
+    /**
+     * http 전송 이력 (기본 100개 까지 메모리 저장)
+     * @return
+     */
+    @Bean
+    public InMemoryHttpExchangeRepository httpExchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
     }
 
 }
